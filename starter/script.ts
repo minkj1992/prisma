a5883b58f37074f5e1f9d34f2dda31d1b2d1da8f
minkj1992 <minkj1992@gmail.com>
2020-12-25 22:18:53 +0900

    my first post query mutation

--- starter/script.ts
+++ starter/script.ts
@@ -4,10 +4,31 @@ const prisma = new PrismaClient();
 
 // A `main` function so that you can use async/await
 async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: "leoo2@gmail.com",
+      name: "leoo",
+    },
+  });
+
+  const post = await prisma.post.create({
+    data: {
+      title: "Leoo's first post with GQL",
+      author: {
+        connect: {
+          email: "leoo2@gmail.com",
+        },
+      },
+    },
+  });
+
   const allUsers = await prisma.user.findMany({
     // eager loading
     include: { posts: true },
   });
+
+  // const results = await Promise.all([post, allUsers]);
+
   console.dir(allUsers, { depth: null });
 }
 
